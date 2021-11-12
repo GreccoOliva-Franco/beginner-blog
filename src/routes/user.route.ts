2e39b57ca42825f69f0927f2a879c8a77626e01c
Franco Grecco <srgrecco93@gmail.com>
2021-11-12 16:33:45 -0300

    Add update method

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,6 +3,9 @@ import userController from '../controllers/user.controller';
 
 const routes = Router();
 
-routes.post('/', userController.create);
+routes
+	.post('/', userController.create)
+	.patch('/', userController.update);
+	// .delete('/', userController.delete);
 
 export default routes;
