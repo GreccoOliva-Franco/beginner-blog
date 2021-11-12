222431753f6b46914f3f24b5d63af993f40b54a7
Franco Grecco <srgrecco93@gmail.com>
2021-11-12 16:31:58 -0300

    Add custom query findByUsername

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,19 +1,10 @@
-import { UserCreate } from './../@types/users.type';
+import { UserCreate, UserUpdate } from './../@types/users.type';
 import { Request, Response } from 'express';
 import { UserService } from '../services/user.service';
 
 class UserController {
-
 	async create(req: Request, res: Response) {
-		const {
-			username,
-			password,
-			name,
-			lastName,
-			email,
-			profileImage,
-			description,
-		} = req.body;
+		const { username, password, name, lastName, email, profileImage, description } = req.body;
 		const user: UserCreate = {
 			username,
 			password,
@@ -27,6 +18,18 @@ class UserController {
 
 		return res.json({
 			message: 'User created',
+			success: true,
+			data: service,
+		});
+	}
+
+	async update(req: Request, res: Response) {
+		const { username, name, lastName, profileImage, description } = req.body;
+		const user: UserUpdate = { username, name, lastName, profileImage, description };
+		const service = await new UserService().update(user);
+
+		return res.json({
+			message: 'User updated',
 			success: true,
 			data: service,
 		});
