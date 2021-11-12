0d863e83fc8cb32da19ba426183dbcddde9fa256
Franco Grecco <srgrecco93@gmail.com>
2021-11-12 16:32:26 -0300

    Add custom query findByUsername

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -4,7 +4,10 @@ import { User } from '../@types';
 
 @EntityRepository(UsersEntity)
 export class UserRepository extends Repository<UsersEntity> {
-
+	async findByUsername(username: string): Promise<User> {
+		const user = await this.findOne({ username });
+		return user;
+	}
 	async findByEmail(email: string): Promise<User> {
 		const user = await this.findOne({ email });
 		return user.userInfo;
