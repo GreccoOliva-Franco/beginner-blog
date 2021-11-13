44a66a042c3b0366d7e2f3853a7d49ac20ddd28c
Franco Grecco <srgrecco93@gmail.com>
2021-11-13 20:03:02 -0300

    Reformat usercontroller.create

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -4,23 +4,32 @@ import { UserService } from '../services/user.service';
 
 class UserController {
 	async create(req: Request, res: Response) {
-		const { username, password, name, lastName, email, profileImage, description } = req.body;
-		const user: UserCreate = {
-			username,
-			password,
-			name,
-			lastName,
-			email,
-			profileImage,
-			description,
-		};
-		const service = await new UserService().create(user);
+		try {
+			const { username, password, name, lastName, email, profileImage, description } = req.body;
+			const user: UserCreate = {
+				username,
+				password,
+				name,
+				lastName,
+				email,
+				profileImage,
+				description,
+			};
+			const userData = await new UserService().create(user);
 
-		return res.json({
-			message: 'User created',
-			success: true,
-			data: service,
-		});
+			return res.json({
+				message: 'User created',
+				success: true,
+				data: userData,
+			});
+		} catch (error) {
+			console.log(error);
+			return {
+				message: "User not created",
+				success: false,
+				error: error,
+			};
+		}
 	}
 
 	async update(req: Request, res: Response) {
