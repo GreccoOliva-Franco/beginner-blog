778635f5447cef80c45c40065be2cb327aba58b0
Franco Grecco <srgrecco93@gmail.com>
2021-11-13 20:09:43 -0300

    Add query parameter to users.delete route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -6,6 +6,6 @@ const routes = Router();
 routes
 	.post('/', userController.create)
 	.patch('/', userController.update)
-	.delete('/', userController.delete);
+	.delete('/:username', userController.delete);
 
 export default routes;
