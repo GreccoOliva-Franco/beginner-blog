ba7938b2c8cb89c0254d53501dd55cbc07eb400e
Franco Grecco <srgrecco93@gmail.com>
2021-11-14 11:53:09 -0300

    Add user.get route

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -4,6 +4,7 @@ import userController from '../controllers/user.controller';
 const routes = Router();
 
 routes
+	.get('/:username', userController.get)
 	.post('/', userController.create)
 	.patch('/', userController.update)
 	.delete('/:username', userController.delete);
