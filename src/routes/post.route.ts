6b3f2593f0121002deb492116448d24efb47f2d5
Franco Grecco <srgrecco93@gmail.com>
2021-11-15 11:42:39 -0300

    Add post.get route

--- src/routes/post.route.ts
+++ src/routes/post.route.ts
@@ -4,6 +4,7 @@ import postController from '../controllers/post.controller';
 const routes = Router();
 
 routes
+	.get('/:id', postController.get)
 	.post('/', postController.create)
 	.patch('/', postController.update)
 	.delete('/:id', postController.delete);
