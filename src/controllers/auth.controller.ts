595c2ab6d7f42cfb5299e14a6306371982b5d090
Franco Grecco <srgrecco93@gmail.com>
2021-11-28 13:46:11 -0300

    Needed commit for sync

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -6,24 +6,24 @@ import passport from 'passport';
 class AuthController {
 	async login(req: Request, res: Response) {
 		/* POST login. */
-			passport.authenticate('local', { session: false }, (err, user, info) => {
-				if (err || !user) {
-					console.log({ info });
-					return res.status(400).json({
-						message: err.message,
-						user,
-					});
-				}
-			   req.login(user, { session: false }, (err) => {
-				   if (err) {
-					   res.send(err);
-				   }
-				   // generate a signed son web token with the contents of user object and return it in the response
-				   const token = new AuthUtil().signJwt(user);
-				   return res.json({ user, token });
+		passport.authenticate('local', { session: false }, (err, user, info) => {
+			if (err || !user) {
+				console.log({ info });
+				return res.status(400).json({
+					message: err.message,
+					user,
 				});
-			})(req, res);
-		};
+			}
+			req.login(user, { session: false }, (err) => {
+				if (err) {
+					res.send(err);
+				}
+				// generate a signed son web token with the contents of user object and return it in the response
+				const token = new AuthUtil().signJwt(user);
+				return res.json({ user, token });
+			});
+		})(req, res);
+	};
 }
 
 export default new AuthController();
